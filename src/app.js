209a464fbf1440d27558f4b20c36e508b5f94a54
Saull Brandão <saull@outlook.com>
2021-03-21 11:27:20 -0300

    Add method to DELETE a repository

--- src/app.js
+++ src/app.js
@@ -33,7 +33,17 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: "Repository does not exist" })
+  }
+
+  repositories.splice(repoIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
